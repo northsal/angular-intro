467dee04497a7795002a6cfc00c32545cb8406fa
northsal <northupst@gmail.com>
2014-11-10 21:40:17 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
 /*
-    app.js: main application script
-    this is an Angular application
+ app.js: main application script
+ this is an Angular application
  */
+
+// angular: model-view-controller
 
 "use strict";
 
+// (name of app, array)
+angular.module('AngularDemo', [])
+    // $scope --> available on any view
+    .controller('HelloController', function($scope) {
+        $scope.person = {
+            firstName: null,
+            lastName: null
+        };
+    })
+
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+        $scope.sortCol = 'rank';
+        $scope.sortBy = function(sortCol) {
+            if ($scope.sortCol == sortCol) {
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+            else {
+                $scope.sortCol = sortCol;
+                $scope.sortReverse = false;
+            }
+        };
+        $scope.isSortedBy = function(colName) {
+            return colName == $scope.sortCol;
+        }
+    });
